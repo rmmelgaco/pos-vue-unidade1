324de11ef262103bb8b672b5a46ceff1e1a8483b
melgacorm <rmmelgaco@gmail.com>
2024-03-12 11:28:50 -0300

    versao inicial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue';
+
+const titulo = ref('Teste rodrigo');
+
+function alteraTitulo() {
+  titulo.value += 1;
+}
+
+</script>
+
+<template>
+  <main>
+    <h2>{{ titulo }}</h2>
+    <button @click='alteraTitulo'>
+      Altera título
+    </button>
+  </main>
+</template>
+
+<style scoped>
+</style>
