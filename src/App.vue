d6413e0d294011f506145aa5a09876536eb9c0d8
melgacorm <rmmelgaco@gmail.com>
2024-03-12 13:58:01 -0300

    mostrando dados

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 
 const sobreMim = ref({})
+let indiceMenuSelecionado = ref(0)
+
+let menus = [
+  'Sobre mim',
+  'Acadêmico',
+  'Profissional',
+  'Contatos'
+]
+
+const tituloPagina = computed(() => menus[indiceMenuSelecionado.value])
+
+function selecionaMenu (indice) {
+  indiceMenuSelecionado.value = indice
+}
 
 fetch('src/meusDados.json')
-    .then( res => res.json())
+    .then(res => res.json())
     .then(data => sobreMim.value = data)
 
 </script>
 
 <template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class='nav'>
+      <li class='nav-item' v-for='(menu, indice) in menus'>
+        <a @click='() => selecionaMenu(indice)'> {{ menu }}</a>
+      </li>
+    </ul>
+    </nav>
+  </header>
   <main>
+    <h1>{{ tituloPagina }}</h1>
     <h2>{{ sobreMim.nome }}</h2>
   </main>
 </template>
