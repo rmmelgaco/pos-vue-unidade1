b69514d7e6a6f23d06e9f59f4a0f6dfe0f46e630
melgacorm <rmmelgaco@gmail.com>
2024-03-13 13:54:24 -0300

    preenchendo dados menus

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,65 @@ fetch('src/meusDados.json')
 </script>
 
 <template>
-  <main>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <ul class='nav'>
-        <li class='nav-item' v-for='(menu, indice) in menus'>
-          <a @click='() => selecionaMenu(indice)'> {{ menu }}</a>
-        </li>
-      </ul>
-    </nav>
+  <header>
+    <div id="nav">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <ul class='nav' >
+          <li class='nav-item greetings' v-for='(menu, indice) in menus' :key="`itemMenu${indice}`">
+            <a @click='() => selecionaMenu(indice)'> {{ menu }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main id='app' style="width: 1300px">
     <h1>{{ tituloPagina }}</h1>
+    <section v-show='indiceMenuSelecionado === 0'>
+      <div class='wrapper'>
+        <div >
+          <p>{{ sobreMim.nome }}</p>
+          <p>{{ sobreMim.descricao }}</p>
+        </div>
+      </div>
+    </section>
+    <section v-show='indiceMenuSelecionado === 1'>
+      <div v-for='(dadoAcademico, indice) in sobreMim.dadosAcademicos' :key="`dadoAcademico${indice}`">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ dadoAcademico.instituicao }}</h5>
+            <p class="card-text">{{ dadoAcademico.curso }}</p>
+            <p class="card-text">{{ dadoAcademico.anoInicio }} - {{ dadoAcademico.anoTermino }}</p>
+          </div>
+        </div>
+        <br/>
+      </div>
+    </section>
+    <section v-show='indiceMenuSelecionado === 2'>
+      <div v-for='(dadoProfissional, indice) in sobreMim.dadosProfissionais' :key="`dadoProfissional${indice}`">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ dadoProfissional.empresaOrgao }}</h5>
+            <p class="card-text">{{ dadoProfissional.cargo }}</p>
+            <p class="card-text">{{ dadoProfissional.periodo }}</p>
+            <p class="card-text">{{ dadoProfissional.detalhes }}</p>
+          </div>
+        </div>
+        <br/>
+      </div>
+    </section>
+    <section v-show='indiceMenuSelecionado === 3'>
+
+    </section>
   </main>
 </template>
 
 <style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
