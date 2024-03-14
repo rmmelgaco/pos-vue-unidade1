283d91d5896532ab78ac14a5125b15878ae0d971
melgacorm <rmmelgaco@gmail.com>
2024-03-14 10:53:27 -0300

    criando chat

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, computed} from 'vue'
+import {ref, reactive, watch, computed} from 'vue'
 
 const sobreMim = ref({})
 let indiceMenuSelecionado = ref(0)
@@ -11,11 +11,44 @@ let menus = [
   'Contatos'
 ]
 
+let mensagens = reactive([])
+let novaMensagem = ref(null)
+let chegouNovaMensagem = ref(false)
+let erro = ref(null)
+
 const tituloPagina = computed(() => menus[indiceMenuSelecionado.value])
 
 function selecionaMenu(indice) {
   indiceMenuSelecionado.value = indice
 }
+
+function enviaMensagem() {
+  const textoMensagem = novaMensagem.value
+  if (textoMensagem) {
+    const dataMensagem = new Date().toLocaleString('pt-BR')
+    mensagens.push({
+      textoMensagem,
+      dataMensagem
+    })
+    novaMensagem.value = null
+  } else {
+    erro.value = 'Digite uma mensagem antes de enviar.'
+  }
+}
+
+function fechaMensagemAlerta() {
+  chegouNovaMensagem.value = false
+}
+
+function fechaMensagemErro() {
+  erro.value = false
+}
+
+watch(() => mensagens.length, (totalMensagensDepois, totalMensagensAntes) => {
+  if (totalMensagensDepois > totalMensagensAntes) {
+    chegouNovaMensagem.value = true
+  }
+})
 
 fetch('src/meusDados.json')
     .then(res => res.json())
@@ -27,7 +60,7 @@ fetch('src/meusDados.json')
   <header>
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <ul class='nav' >
+        <ul class='nav'>
           <li class='nav-item greetings' v-for='(menu, indice) in menus' :key="`itemMenu${indice}`">
             <a @click='() => selecionaMenu(indice)'> {{ menu }}</a>
           </li>
@@ -37,11 +70,20 @@ fetch('src/meusDados.json')
   </header>
   <main id='app' style="width: 1300px">
     <h1>{{ tituloPagina }}</h1>
+    <div v-if='chegouNovaMensagem' class="alert alert-warning" role="alert">
+      Chegou uma nova mensagem no chat.
+      <a @click='fechaMensagemAlerta'>X</a>
+    </div>
+    <div v-if='erro' class="alert alert-danger" role="alert">
+      {{ erro }}
+      <a @click='fechaMensagemErro'>X</a>
+    </div>
     <section v-show='indiceMenuSelecionado === 0'>
       <div class='wrapper'>
-        <div >
-          <p>{{ sobreMim.nome }}</p>
-          <p>{{ sobreMim.descricao }}</p>
+        <div>
+          <h3>{{ sobreMim.nome }}</h3>
+          <br />
+          <h6>{{ sobreMim.descricao }}</h6>
         </div>
       </div>
     </section>
@@ -76,9 +118,10 @@ fetch('src/meusDados.json')
           <div class="card-body">
             <h5 class="card-title">{{ contato.texto }}</h5>
             <p class="card-text">
-              <a target='_blank' v-if="contato.tipo === 'link'" v-bind:href="contato.contato">{{contato.contato}}</a>
-              <a target='_blank' v-else-if="contato.tipo === 'email'" v-bind:href="`mailTo:${contato.contato}`">{{contato.contato}}</a>
-              <span v-else>{{ contato.contato}}</span>
+              <a target='_blank' v-if="contato.tipo === 'link'" v-bind:href="contato.contato">{{ contato.contato }}</a>
+              <a target='_blank' v-else-if="contato.tipo === 'email'"
+                 v-bind:href="`mailTo:${contato.contato}`">{{ contato.contato }}</a>
+              <span v-else>{{ contato.contato }}</span>
             </p>
           </div>
         </div>
@@ -86,6 +129,19 @@ fetch('src/meusDados.json')
       </div>
     </section>
   </main>
+  <footer>
+    <div class='card'>
+      <h4 class='tituloMensagemChat'>Mensagens</h4>
+      <p v-if='!mensagens.length'>Nenhuma mensagem recebida.</p>
+      <span v-for='(mensagem, indice) in mensagens' :key="`mensagem${indice}`">
+      <p>{{ mensagem.textoMensagem }} - {{ mensagem.dataMensagem }}</p>
+    </span>
+      <div>
+        <textarea class="form-control" v-model='novaMensagem'/>
+        <button type='button' class="btn btn-primary" @click='enviaMensagem'>Enviar</button>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
